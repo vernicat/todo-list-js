e0cb446f6ddcc4f737b66b3b8db676e1757d970e
vernicat <catherine.vernigorova@gmail.com>
2021-04-23 02:00:48 +0300

    add firebase

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,13 +1,38 @@
 const todo = () => {
+    let todoListDb = {};
     
-    // конструктор
+    // Конструктор
     const construct = () => {
+        dbConnect();
         selectElements();
+        buildTask();
         bindAdd();
-        bindTodoListItem();
     };
 
-    // выбор элементов
+    // Подключение к БД
+    const dbConnect = () => {
+        firebase.initializeApp(firebaseConfig);
+        db = firebase.database();
+    };
+
+    // Получение списка задач
+    const getListTasks = async () => {
+        await db.ref().child('tasks').get().then((snapshot) => {
+            todoListDb = snapshot.val();
+        });
+    };
+
+    // Добавление задачи в БД
+    const addTaskDb = async text => {
+        let postListRef = db.ref('tasks');
+        let newPostRef = postListRef.push();
+        newPostRef.set({
+            taskText: text,
+            completed: false
+        });
+    };
+
+    // Выбор элементов
     const selectElements = () => {
         todoInput = document.getElementById("todo-input");
         todoList = document.getElementById("todo-list");
@@ -16,93 +41,107 @@ const todo = () => {
         infoMessage = document.getElementById("message");
     };
 
-    // рендеринг задачи
-    const buildTask = () => {
+    // Рендеринг задач
+    const buildTask = async () => {
         let todoListItem, taskCheckbox, taskValue, taskButton;
 
-        todoListItem = document.createElement("li");
-        todoListItem.setAttribute("class", "task");
+        await getListTasks();
+        todoList.innerHTML = "";
 
-        taskCheckbox = document.createElement("input");
-        taskCheckbox.setAttribute("type", "checkbox");
+        if (!todoListDb) return;
 
-        taskValue = document.createElement("span");
-        taskValue.innerHTML = this.todoInput.value;
+        Object.entries(todoListDb).map(([key, task]) => {
+            todoListItem = document.createElement("li");
+            todoListItem.setAttribute("class", "task");
+            
+            taskCheckbox = document.createElement("input");
+            taskCheckbox.setAttribute("type", "checkbox");
+            taskCheckbox.checked = task.completed;       
+            completeTask(todoListItem,taskCheckbox, key);
 
-        taskButton = document.createElement("button");
-        taskButton.setAttribute("class", "fa fa-trash");
+            taskValue = document.createElement("span");
+            taskValue.innerHTML = task.taskText;
+    
+            taskButton = document.createElement("button");
+            taskButton.setAttribute("class", "fa fa-trash");
+    
+            todoListItem.appendChild(taskCheckbox);
+            todoListItem.appendChild(taskValue);
+            todoListItem.appendChild(taskButton);
+    
+            todoList.appendChild(todoListItem);
+        });
 
-        todoListItem.appendChild(taskCheckbox);
-        todoListItem.appendChild(taskValue);
-        todoListItem.appendChild(taskButton);
-
-        todoList.appendChild(todoListItem);
+        await bindTodoListItem();
     };
 
-    // отображение ошибки
+    // Отображение ошибки
     const error = () => { 
         infoMessage.style.display = "block";
     };
 
-    // добавление задачи
-    const addTask = () => {
+    // Добавление задачи
+    const addTask = async () => {
         let taskValue = todoInput.value;
         infoMessage.style.display = "none";
 
         if (taskValue === "")
             error();
         else {
-            buildTask();
+            await addTaskDb(taskValue);
+            await buildTask();
             todoInput.value = "";
-            bindTodoListItem();
         }
     };
 
-    // добавление по Enter
+    // Добавление задачи по Enter
     const enterKey = event => {
         if (event.keyCode === 13 && event.which === 13)
             addTask();
     };
 
-    // прикрепление обработчика для добавления
+    // Прикрепление обработчика для добавления
     const bindAdd = () => {
         addButton.onclick = addTask.bind(this);
         todoInput.onkeypress = enterKey.bind(this);
     };
 
-    // прикрепление обработчика для списка
-    const bindTodoListItem = () => {
-        let todoListItem, checkBox, deleteButton;
+    // Прикрепление обработчика для списка
+    const bindTodoListItem = async () => {
+        let todoListItem, checkBox, deleteButton, idx = 0;
 
-        for (i = 0; i < todoListChildren.length; i++) {
-            todoListItem = todoListChildren[i];
+        Object.entries(todoListDb).map(([key]) => {
+            todoListItem = todoListChildren[idx++];
 
             checkBox = todoListItem.getElementsByTagName("input")[0];
             deleteButton = todoListItem.getElementsByTagName("button")[0];
 
-            checkBox.onclick = completeTask.bind(this, todoListItem, checkBox);
+            checkBox.onclick = completeTask.bind(this, todoListItem, checkBox, key);
 
-            deleteButton.onclick = deleteTask.bind(this, i);
-        }
+            deleteButton.onclick = deleteTask.bind(this, key);
+        })
     };
 
-    // удаление задачи
-    const deleteTask = i => {
-        todoListChildren[i].remove();
-        bindTodoListItem();
+    // Удаление задачи
+    const deleteTask = async key => {
+        await db.ref().child('tasks/' + key).remove();
+        await buildTask();
     };
 
-    // завершение задачи
-    const completeTask = (todoListItem, checkBox) => {
-        if (checkBox.checked)
+    // Изменение статуса задачи на "Выполнено"
+    const completeTask = (todoListItem, checkBox, key) => {
+        if (checkBox.checked) {
             todoListItem.className = "task completed";
+            db.ref().child('tasks/' + key).update({ 'completed': true })
+        }
         else
-            incompleteTask(todoListItem);
+            incompleteTask(todoListItem, key);
     };
 
-    // перенос задачи обратно в выполнение
-    const incompleteTask = todoListItem => {
+    // Изменение статуса задачи на "Не выполнено"
+    const incompleteTask = (todoListItem, key) => {
         todoListItem.className = "task";
+        db.ref().child('tasks/' + key).update({ 'completed': false })
     };
 
     construct();
